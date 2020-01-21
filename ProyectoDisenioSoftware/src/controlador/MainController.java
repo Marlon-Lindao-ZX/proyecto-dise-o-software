ba637c0c77a30225fb56a94eca41062b2b548f12
Kevin Cevallos <keanceva@espol.edu.ec>
2020-01-21 13:22:20 -0500

    Modificaciones imcompletas

--- ProyectoDisenioSoftware/src/controlador/MainController.java
+++ ProyectoDisenioSoftware/src/controlador/MainController.java
@@ -5,12 +5,18 @@
  */
 package controlador;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -20,11 +26,38 @@ public class MainController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private Button button;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+    }
+    @FXML
+    private void handleButton2Action(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    @FXML
+    private void handleButton3Action(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    @FXML
+    private void handleButton4Action(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("Main1.fxml"));
+
+            Scene escena = new Scene(root);
+            stage.setScene(escena);
+            stage.show();
     }
     
     @Override
--- ProyectoDisenioSoftware/src/vista/Main.java
+++ ProyectoDisenioSoftware/src/vista/Main.java
@@ -9,6 +9,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -26,7 +28,6 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
     /**
      * @param args the command line arguments
      */
